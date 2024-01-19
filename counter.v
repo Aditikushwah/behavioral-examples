8bb7194cb75a94fc4a904ba1a380beeaf29de25d
Aditikushwah <99320349+Aditikushwah@users.noreply.github.com>
2024-01-20 00:55:51 +0530

    Update counter.v

--- counter.v
+++ counter.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11.01.2024 14:41:07
-// Design Name: 
-// Module Name: counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module counter(
     input clk,
     input ld,
