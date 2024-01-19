7a142b4c3d0f37bf094ae02298cacd328dd460e9
Aditikushwah <99320349+Aditikushwah@users.noreply.github.com>
2024-01-19 23:04:37 +0530

    Update ha.v

--- ha.v
+++ ha.v
@@ -1,39 +1,13 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.01.2024 00:28:46
-// Design Name: 
-// Module Name: ha
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module ha(input a,b,
 output s,c
 
     );
     assign s=a^b;
     assign c=a|b;
-    
+    endmodule
 
-    
-    
-    
-endmodule
-
-
+/////////////////usung parameter
 module hap(input a1,b1,
 output s1,c1
 
